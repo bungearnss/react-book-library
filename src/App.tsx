38267cdeaf00ba69ad1d51c9e8325c85c80e02f6
bungearntun <love2554f@hotmail.com>
2025-04-06 12:27:07 +0700

    updated : app routing using NavLink and Link

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,26 @@ import "./App.css";
 import { NavBar } from "./layouts/NavBar";
 import { Footer } from "./layouts/Footer";
 import { HomePage } from "./layouts/homepage/HomePage";
+import { SearchBooksPage } from "./layouts/searchbook/SearchBooksPage";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className="d-flex flex-column min-vh-100">
       <NavBar />
-      <HomePage />
+      <div className="flex-grow-1">
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home">
+            <HomePage />
+          </Route>
+          <Route path="/search">
+            <SearchBooksPage />
+          </Route>
+        </Switch>
+      </div>
       <Footer />
     </div>
   );
